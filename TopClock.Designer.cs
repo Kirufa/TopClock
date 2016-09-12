dea81dabaf33f6d2dd6fdfb0d2bb7c125d247aa9
Kirufa <yushuen0331@gmail.com>
2016-09-12 11:30:48 +0800

    first commit

---
+++ TopClock.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace Clock
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置 Managed 資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.timeLbl = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // timeLbl
+            // 
+            this.timeLbl.AutoSize = true;
+            this.timeLbl.Font = new System.Drawing.Font("Cooper Black", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.timeLbl.ForeColor = System.Drawing.Color.Black;
+            this.timeLbl.Location = new System.Drawing.Point(0, 0);
+            this.timeLbl.Margin = new System.Windows.Forms.Padding(3);
+            this.timeLbl.Name = "timeLbl";
+            this.timeLbl.Size = new System.Drawing.Size(187, 38);
+            this.timeLbl.TabIndex = 0;
+            this.timeLbl.Text = "06:30:21.5";
+            this.timeLbl.SizeChanged += new System.EventHandler(this.timeLbl_SizeChanged);
+            this.timeLbl.DoubleClick += new System.EventHandler(this.timeLbl_DoubleClick);
+            this.timeLbl.MouseClick += new System.Windows.Forms.MouseEventHandler(this.timeLbl_MouseClick);
+            this.timeLbl.MouseDown += new System.Windows.Forms.MouseEventHandler(this.timeLbl_MouseDown);
+            this.timeLbl.MouseLeave += new System.EventHandler(this.timeLbl_MouseLeave);
+            this.timeLbl.MouseMove += new System.Windows.Forms.MouseEventHandler(this.timeLbl_MouseMove);
+            this.timeLbl.MouseUp += new System.Windows.Forms.MouseEventHandler(this.timeLbl_MouseUp);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(556, 111);
+            this.Controls.Add(this.timeLbl);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "MainForm";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.MainForm_FormClosing);
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label timeLbl;
+    }
+}
+
---
+++ TopClock.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Timers;
+using System.Drawing;
+
+using Timer = System.Timers.Timer;
+
+namespace Clock
+{
+    public partial class MainForm : Form
+    {
+        private Timer timer;
+        private const int interval = 10;
+        private bool isDown;
+        private Point point;
+
+        public MainForm()
+        {
+
+            InitializeComponent();
+            timer = new Timer();
+            timer.Interval = interval;
+            timer.Elapsed += new ElapsedEventHandler(Timer_Elapsed);
+            timer.SynchronizingObject = this;
+
+            timer.Start();
+
+            this.TopMost = true;
+            isDown = false;
+
+            point = new Point();
+
+        }
+
+        private void Timer_Elapsed(object sender, ElapsedEventArgs e)
+        {
+            string timestamp = DateTime.Now.ToString("HH:mm:ss.f");
+            timeLbl.Text = timestamp;           
+        }
+
+        private void timeLbl_MouseClick(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Right)
+            {
+                timer.Stop();
+                this.Close();
+                Application.Exit();
+            }            
+        }
+
+     
+
+        private void timeLbl_SizeChanged(object sender, EventArgs e)
+        {
+            this.Size = timeLbl.Size;           
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            timeLbl_SizeChanged(null, null);
+        }
+
+        private void timeLbl_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (isDown)
+                this.Location = Point.Subtract(Cursor.Position, new Size(point));
+        }
+
+        private void timeLbl_MouseLeave(object sender, EventArgs e)
+        {
+            isDown = false;
+        }
+
+        private void timeLbl_MouseUp(object sender, MouseEventArgs e)
+        {
+            isDown = false;
+        }
+
+        private void timeLbl_MouseDown(object sender, MouseEventArgs e)
+        {
+            isDown = true;
+            point.X = Cursor.Position.X - this.Location.X;
+            point.Y = Cursor.Position.Y - this.Location.Y;
+
+        }
+
+        private void timeLbl_DoubleClick(object sender, EventArgs e)
+        {
+            this.Location = new Point((Screen.FromControl(this).Bounds.Width - this.Width) / 2, this.Location.Y);
+        }
+
+        private void MainForm_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            timer.Stop();
+        }
+    }
+}
